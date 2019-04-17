fa889960f84260f8053cddcb1b26998bc20c7a95
Alvardud <alvaro_dudutex@outlook.es>
2019-04-16 21:38:48 -0400

    delete issues

--- iris_challenge/index.js
+++ iris_challenge/index.js
@@ -58,10 +58,6 @@ model.compile({
     loss: "categoricalCrossentropy",
     optimizer: tf.train.adam()
 })
-/*model.compile({
-    loss: "categoricalCrossentropy",
-    optimizer: tf.train.adam()
-})*/
 
 //creando el algoritmo que nos mostrara en pantalla, cada iteracion, mostrando su perdida durante la historia (cambio)
 
@@ -85,56 +81,4 @@ async function main(){
 }
 
 main();
-/*
 
- // creating model
- const outputData = tf.tensor2d(irisTraining.map(item => [
-     item.species === 'setosa' ? 1 : 0,
-     item.species === 'virginica' ? 1 : 0,
-     item.species === 'versicolor' ? 1 : 0
- 
- ]), [130,3])
- 
- // Creating Model
- const model = tf.sequential();
- 
- 
- model.add(tf.layers.dense(
-     {   inputShape: 4, 
-         activation: 'sigmoid', 
-         units: 10
-     }
- ));
- 
- model.add(tf.layers.dense(
-     {
-         inputShape: 10, 
-         units: 3, 
-         activation: 'softmax'
-     }
- ));
- 
- model.summary();
- 
- // compiling model
- model.compile({
-     loss: "categoricalCrossentropy",
-     optimizer: tf.train.adam()
- })
- 
- async function train_data(){
-     console.log('......Loss History.......');
-     for(let i=0;i<40;i++){
-      let res = await model.fit(trainingData, outputData, {epochs: 40});
-      console.log(`Iteration ${i}: ${res.history.loss[0]}`);
-   }
- }
- 
- async function main() {
-     await train_data();
-     console.log('....Model Prediction .....')
-     model.predict(testingData).print();
- }
- 
- main();
-   */
